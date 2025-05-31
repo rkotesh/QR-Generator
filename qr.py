ef307057c990330241d5d76e0b553995a9878f01
S. koteswararao <srkotesh23@gmail.com>
2025-05-31 16:22:45 +0530

    Add files via upload

--- qr.py
+++ qr.py
@@ -1,10 +1,17 @@
-
+import streamlit as st
 import pyqrcode
-import os
-
 from PIL import Image
-link = input("Enter the link: ")
-qr_code = pyqrcode.create(link)
-qr_code.png("qr_code.png", scale=6)
-img = Image.open("qr_code.png")
-os.startfile("qr_code.png")
+
+st.title("QR Code Generator")
+
+link = st.text_input("Enter a link : ")
+if link:
+    qr_code = pyqrcode.create(link)
+    qr_code.png("qr_code.png", scale=6)
+    
+    img = Image.open("qr_code.png")
+    st.image(img)
+    st.success("QR Code generated succesfully")
+else:
+    st.error("please enter a valid link")
+    
